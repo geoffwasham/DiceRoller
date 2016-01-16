dc0abe0e072499d55c99a56978c7b7a5644a3e08
Geoff Washam <geoff@prolificagency.com>
2016-01-16 13:57:47 -0600

    Install Normalize.css

--- index.php
+++ index.php
@@ -11,6 +11,7 @@
 
 	<title>DiceRoller</title>
 
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<link href="css/bootstrap.min.css" rel="stylesheet">
 
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
