7dd7f60fbfe043f8c293b18dcb9b5780347663e3
Geoff Washam <geoff@prolificagency.com>
2016-01-16 14:31:37 -0600

    Build basis of dice input form

--- index.php
+++ index.php
@@ -18,6 +18,44 @@
     <script src="js/bootstrap.min.js"></script>
 </head>
 <body>
-Testing
+<div class="container">
+	<div class="row">
+		<div class="col-md-12">
+			<header class="main-header">
+				<h1>Dice Roller</h1>
+				<br>
+				<h2>by Geoff</h2>
+			</header>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-md-6">
+			<section id="dice_form">
+				<header class="section-header">
+					<h3>Dice form here</h3>
+				</header>
+				<article>
+					<form>
+						<table>
+							<tr>
+								<td><label for="dice_qty_1"># of<br>dice</label></td>
+								<td><label for="dice_type_1">Dice Type</label></td>
+							</tr>
+							<tr>
+								<td><input type="number" value="1" min="0" id="dice_qty_1" name="dice_qty_1"></td>
+								<td>
+									<select id="dice_type_1" name="dice_type_1">
+										<option value="6">d6</option>
+									</select>
+								</td>
+							</tr>
+						</table>
+						<input type="reset" value="Reset"> <input type="submit" value="Roll!">
+					</form>
+				</article>
+			</section>
+		</div>
+	</div>
+</div>
 </body>
 </html>
